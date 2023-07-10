7967ec9051ecf7358aeba69d3bd6b74da966ce74
THUIRT <thuirt@thuirt.com>
2023-07-10 22:19:17 +0800

    Add more interfaces with huansic's data

--- SmartCar_CM_LVGL_Demo/SmartCar_CM_LVGL_Demo/LVGL_GUI/drivers/ILI9341_Porting.h
+++ SmartCar_CM_LVGL_Demo/SmartCar_CM_LVGL_Demo/LVGL_GUI/drivers/ILI9341_Porting.h
@@ -9,6 +9,10 @@
 #define LVGL_GUI_DRIVERS_ILI9341_PORTING_H_
 #include "debug.h"
 
+extern float ui_Kp;
+extern float ui_Ki;
+extern float ui_Kd;
+
 /* 已在CH32V307平台实现的代码  */
 void LVGL_TIM2_Init(void);
 void TIM2_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -24,5 +28,17 @@ void WCH_ILI9341_GPIO_FullDuplex_Init(void);
 void ui_cmd_go(void);
 /* 紧急停车 */
 void ui_cmd_stop(void);
+/* 更新Kp */
+void ui_display_Kp(float data);
+/* 更新Ki */
+void ui_display_Ki(float data);
+/* 更新Kd */
+void ui_display_Kd(float data);
+/* 更新速度 */
+void ui_display_speed(float data);
+/* 更新PID目标值 */
+void ui_display_pid_goal(float data);
+/* 更新当前状态 */
+void ui_display_step_counter(uint8_t* data);
 
 #endif /* LVGL_GUI_DRIVERS_ILI9341_PORTING_H_ */
