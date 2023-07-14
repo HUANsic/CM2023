ee95df6a188145eaf20120395de7f1088f066dbc
THUIRT <thuirt@thuirt.com>
2023-07-14 14:39:09 +0800

    Disabled the default fonts to shorten the .data

--- SmartCar_CM_LVGL_Demo/SmartCar_CM_LVGL_Demo/LVGL_GUI/lvgl/lv_conf.h
+++ SmartCar_CM_LVGL_Demo/SmartCar_CM_LVGL_Demo/LVGL_GUI/lvgl/lv_conf.h
@@ -364,7 +364,7 @@
 #define LV_FONT_MONTSERRAT_8  0
 #define LV_FONT_MONTSERRAT_10 0
 #define LV_FONT_MONTSERRAT_12 0
-#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_14 0
 #define LV_FONT_MONTSERRAT_16 0
 #define LV_FONT_MONTSERRAT_18 0
 #define LV_FONT_MONTSERRAT_20 0
@@ -399,7 +399,8 @@
 #define LV_FONT_CUSTOM_DECLARE
 
 /*Always set a default font*/
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+//#define LV_FONT_DEFAULT &lv_font_montserrat_8
+#define LV_FONT_DEFAULT &lv_font_Alatsi_Regular_14
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
